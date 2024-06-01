d01e418310c9b44d796bffe63f36d1a01b858a43
Jhosymar Cortes Rodriguez <cortejhosymar@gmail.com>
2024-05-31 23:54:58 -0600

    Primeros avances en el portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SoyComponent } from './componest/soy/soy.component';
+import { ProyectosComponent } from './componest/proyectos/proyectos.component';
+import { ContactameComponent } from './componest/contactame/contactame.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'QuienSoy',component:SoyComponent},
+  {path:'Proyectos', component:ProyectosComponent},
+  {path:'Contactame', component:ContactameComponent},
+  {path:'**',redirectTo:'QuienSoy'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProyectosComponent } from './componest/proyectos/proyectos.component';
+import { ContactameComponent } from './componest/contactame/contactame.component';
+import { SoyComponent } from './componest/soy/soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProyectosComponent,
+    ContactameComponent,
+    SoyComponent
   ],
   imports: [
     BrowserModule,
